edb4beac9cf7fb00760c4b55355cf6665e788da5
romchek7 <sonecccbbbaaa@gmail.com>
2021-01-05 13:39:58 +0200

    New commit 2 2021
    fix products elements

--- js/menu.js
+++ js/menu.js
@@ -1,31 +1,16 @@
-//робота меню для мобільних пристроїв
-const navSlide = () => {
-    const burger = document.getElementById("burger");
-    const nav = document.querySelector('.nav_ul');
-    const navLinks = document.querySelectorAll('.nav_ul li');
+var menuBTN = document.getElementById("burger");
 
-    //відкриття меню
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
+var navMenu = document.getElementById("phoneMenu");
+var aText = document.getElementById("aPhoneID");
 
-        //показ посилань у меню
-        navLinks.forEach((link, index) => {
-            //умова для анімації посилань кожного разу
-            if (link.style.animation) {
-                link.style.animation = '';
-                burger.style.color = '';
-            }
-            else {
-                link.style.animation = `navLinkFade 1s ease forwards ${index / 7 + 0.3}s`;
-                burger.style.color = "#E60F0F";
-            }
-        });
-    });
+function showNavMenu() {
+    if (navMenu.style.display == "none") {
+        navMenu.style.display = "flex";
+        aText.style.color = "#E60F0F";
+    }
+    else {
+        navMenu.style.display = "none";
+        aText.style.color = "";
+    }
 }
-navSlide();
-
-//зміна коліру панелі при прокрутці
-window.addEventListener('scroll', function () {
-    let nav = document.querySelector("nav");
-    nav.classList.toggle('scrolling', window.scrollY > 0); //window.scrollY > 120 - це позиція вікна
-})
+menuBTN.addEventListener("click", showNavMenu);
